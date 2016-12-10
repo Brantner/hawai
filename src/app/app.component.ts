572e204a081f2a297b37b2bdc084bee84801ad24
brantner <zamula.dmitry@gmail.com>
2016-12-10 23:38:24 +0300

    Finish markup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import {Component} from "@angular/core";
 import "moment/locale/ru.js";
+import * as moment from "moment";
 
 @Component({
   selector: 'app-root',
@@ -7,5 +8,26 @@ import "moment/locale/ru.js";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  selectableDates: Array<{date: Date, mode: string, clazz: string}> = [];
+
+  constructor() {
+    const startOfMonth = moment().startOf('month');
+    const endOfAccessiblePeriod = moment().isAfter(moment().day('Wednesday'))
+      ? moment().endOf('week').add('1', 'week')
+      : moment().endOf('week');
+
+
+    let day = startOfMonth;
+    while (day.isSameOrBefore(endOfAccessiblePeriod)) {
+      if (this.isWeekend(day)) {
+        this.selectableDates.push({date: day.toDate(), mode: 'day', clazz: 'selectable-day'});
+      }
+      day.add('1', 'day');
+    }
+  }
+
+  private isWeekend(day: moment.Moment) {
+    return (day.day() != 6) && (day.day() != 0);
+  }
 }
